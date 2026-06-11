d781b8855d10727916fefd7702201b047e21e32a
Anderson de Matos Guimarães <anderson.m.guimaraes@aluno.senai.br>
2026-06-11 16:57:41 -0300

    feat: cria a entidade autor

--- motorartigos/models.py
+++ motorartigos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Aqui vou criar minhas classes de entidade
+
+# 1. Primeiro criamos a entidade Autor
+class Autor(models.Model):
+    # atributo
+    # O atributo 'id' é automático (no django).
+    # Chave primária: imutável, universal e única
+    nome = models.CharField(max_lenght=100)
+    biografia = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+
+    
