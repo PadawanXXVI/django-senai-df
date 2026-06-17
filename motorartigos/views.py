fe1cd6f8bf1b8ee7860da70ea745e1f22bf95d84
Anderson de Matos Guimarães <anderson.m.guimaraes@aluno.senai.br>
2026-06-17 15:44:02 -0300

    ativa a página index.html

--- motorartigos/views.py
+++ motorartigos/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse # incluído para criar a maninupar 'respostas' HTTP
 
 # Create your views here.
 # Aqui vou criar minhas rotas
 # Minhas regra de negócio
+
+def index(request):
+    # return HttpResponse("<h1>Oi</h1>")
+    return render(request, 'motorartigos/index.html') # ativa a página index.html
