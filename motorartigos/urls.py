d85aa82ec8ded628476103fed7fd051537c96cea
Anderson de Matos Guimarães <anderson.m.guimaraes@aluno.senai.br>
2026-06-17 15:43:21 -0300

    cria as rotas do app

--- motorartigos/urls.py
+++ motorartigos/urls.py
@@ -1,2 +1,9 @@
 # Boa prática
 # Cada app se vira com suas rotas
+
+from django.urls import path # incluído
+from motorartigos.views import index # incluído
+
+urlpatterns = [
+    path('', index), # define a rota do próprio app
+]
