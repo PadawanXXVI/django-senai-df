1f117634b0e962f36d8ed6007afe50e50ace5d09
Anderson de Matos Guimarães <anderson.m.guimaraes@aluno.senai.br>
2026-06-26 15:47:02 -0300

    conforme gabarito

--- motorartigos/urls.py
+++ motorartigos/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from motorartigos.views import index, artigo
 
 urlpatterns = [
-    path('',index,name='index'),
-    path('artigo/',artigo,name='artigo')
+    path('',index, name='index'),
+    path('artigo/',artigo, name='artigo')
  ]
